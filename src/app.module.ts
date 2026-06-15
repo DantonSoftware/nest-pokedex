78a2ba15c7532a53acf4f1e706e59a194b6e4b1f
Danton Cortes <danton.cortes@gmail.com>
2026-06-15 11:30:24 -0400

    subir ultimos cambios de configuracion

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,17 +5,35 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/jooi.validation';
+import * as dns from 'dns';
+
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
     }),
-    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    MongooseModule.forRoot(process.env.MONGODB,{
+      dbName: 'pokemonsdb'  
+    }),
     PokemonModule,
     CommonModule,
     SeedModule 
   ],
 
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private readonly configService: ConfigService) {
+    console.log('MONGODB desde ConfigService:', this.configService.get('MONGODB'));
+    console.log('PORT desde ConfigService:', this.configService.get('PORT'));
+  }
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,14 +5,22 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly configService: ConfigService,
+  ){
+    // console.log(process.env.DEFAULT_LIMIT);
+    this.defaultLimit = configService.get<number>('defaultLimit');
+    // console.log(configService.get<number>('defaultLimit'));
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -29,7 +37,7 @@ export class PokemonService {
 
   findAll(paginationDto: PaginationDto) {
 
-    const { limit = 10, offset = 0 } = paginationDto;
+    const { limit = this.defaultLimit, offset = 0 } = paginationDto;
 
     return this.pokemonModel.find()
     .limit(limit)
